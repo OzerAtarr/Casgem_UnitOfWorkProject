6535fc6a025870e747bcd9cff9d340c2f7a5b7ff
90536 <90536@DESKTOP-FCHRJ42>
2023-07-26 14:17:29 +0300

    controller

---
+++ CasgemUow.PresentationLayer/Controllers/DefaultController.cs
@@ -0,0 +1,43 @@
+﻿using CasgemUow.BusinessLayer.Abstract;
+using CasgemUow.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CasgemUow.PresentationLayer.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private readonly ICustomerProcessService _customerProcessService;
+        private readonly ICustomerService _customerService;
+        public DefaultController(ICustomerProcessService customerProcessService, ICustomerService customerService)
+        {
+            _customerProcessService = customerProcessService;
+            _customerService = customerService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Index(CustomerProcess customerProcess)
+        {
+            var valueSender = _customerService.TGetByID(customerProcess.SenderID);
+            var valueReciver = _customerService.TGetByID(customerProcess.ReceiverID);
+
+            valueReciver.CustomerBalance += customerProcess.Amount;
+            valueSender.CustomerBalance -= customerProcess.Amount;
+
+            List<Customer> modifiedCustomer = new List<Customer>()
+            {
+                valueSender,
+                valueReciver
+            };
+
+            _customerService.TMultiUpdate(modifiedCustomer);
+            return RedirectToAction("CustomerProcessList");
+        }
+    }
+}
